09f2a429b67fc40252eece7d8be20765237f248c
Jason <429978+spaceyjase@users.noreply.github.com>
2021-06-07 16:12:59 +0100

    Creating the Battler.

---
+++ Scenes/Battler.cs
@@ -0,0 +1,94 @@
+using Godot;
+using System;
+
+public class Battler : Node2D
+{
+  [Export] private Resource stats;
+  [Export] private PackedScene aiScene;
+  [Export] private Array actions;
+  [Export] private bool isPartyMember;
+  
+  // Emitted when changing 'isSelected'. The UI will react to this for
+  // player controlled battlers.
+  [Signal] public delegate void SelectionToggled(bool value);
+  [Signal] public delegate void ReadyToAct();
+  [Signal] public delegate void ReadinessChanged(float newReadiness);
+  
+  private bool isActive = true;
+  private float readiness;
+  private bool isSelected;
+  private bool isSelectable;
+  
+  // The turn queue will change this property when another battler is acting
+  public float TimeScale { get; set; } = 1.0f;
+  
+  // When this value reaches 100, the battler is ready to take their turn
+  private float Readiness
+  {
+    get => readiness;
+    set
+    {
+      readiness = value;
+      EmitSignal(nameof(ReadinessChanged), readiness);
+
+      if (readiness < 100f) return;
+      
+      EmitSignal(nameof(ReadyToAct));
+      
+      // pause the process loop now this battler is ready
+      SetProcess(false);
+    }
+  }
+  
+  public bool IsActive
+  {
+    get => isActive;
+    set
+    {
+      isActive = value;
+      SetProcess(isActive);
+    }
+  }
+
+  private bool IsSelected
+  {
+    get => isSelected;
+    set
+    {
+      if (value && !isSelectable)
+      {
+        GD.Print("Battler isn't selectable!");
+      }
+
+      isSelected = value;
+      EmitSignal(nameof(SelectionToggled), isSelected);
+    }
+  }
+
+  public bool IsSelectable
+  {
+    get => isSelectable;
+    set
+    {
+      isSelectable = value;
+      if (!isSelectable)
+      {
+        IsSelected = false;
+      }
+    }
+  }
+
+  public bool IsPlayerControlled => aiScene == null;
+
+  public override void _Process(float delta)
+  {
+    base._Process(delta);
+
+    Readiness = Readiness + stats.Speed * delta * TimeScale;
+  }
+
+  public void SetTimeScale(float newScale)
+  {
+    TimeScale = newScale;
+  }
+}
