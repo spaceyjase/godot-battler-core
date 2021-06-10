3b268e37b08af69bf2e0c80fb75df73ad1d5c7c8
Jason <429978+spaceyjase@users.noreply.github.com>
2021-06-10 13:08:17 +0100

    Implementing damage formulas WIP.

--- Scripts/Formulas/Formulas.cs
+++ Scripts/Formulas/Formulas.cs
@@ -41,5 +41,38 @@ namespace battler.Scripts.Formulas
 
       return multiplier;
     }
+
+    // Hit chance is (attacker.HitChance - defender.Evasion) * actionHitChance + affinity bonus + 
+    // element tried bonus - defender affinity bonus.
+    private static float CalculateHitChance(AttackActionData data, Battler attacker, Battler defender)
+    {
+      var chance = attacker.Stats.HitChance - defender.Stats.Evasion;
+      // Hit chance is a value between 0 and 100 for consistency with the battler's stats
+      // as it is used as a multiplier here, we need to divide by 100 first.
+      chance *= data.HitChance / 100f;
+      
+      // Use the affinity and weaknesses to apply a hit chance bonus or penalty.
+      var element = data.Element;
+      if (element == attacker.Stats.Affinity)
+      {
+        chance += 5f; // TODO: magic number
+      }
+
+      if (element != Elements.None)
+      {
+        // if the action's element is part of the defender's weaknesses, increase the hit rating by 10.
+        if (defender.Stats.Weaknesses.Any(e => e == Types.WeaknessMapping[element]))
+        {
+          chance += 10f; // TODO: magic
+        }
+        // However, if the defender has an affinity with the action's element, decrease the hit rating by 10.
+        if (Types.WeaknessMapping[defender.Stats.Affinity] == element)
+        {
+          chance -= 10f;
+        }
+      }
+
+      return Mathf.Clamp(chance, 0f, 100f);
+    }
   }
 }
