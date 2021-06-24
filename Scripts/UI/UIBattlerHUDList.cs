75ded7008d6a280b04af7ad50450f8cb41dd55eb
Jason <429978+spaceyjase@users.noreply.github.com>
2021-06-24 16:13:28 +0100

    Battler HUD WIP.

---
+++ Scripts/UI/UIBattlerHUDList.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using Godot;
+
+namespace battler.Scripts.UI
+{
+  public class UIBattlerHUDList : VBoxContainer
+  {
+    private readonly PackedScene uiBattlerHud = ResourceLoader.Load<PackedScene>($"{nameof(UIBattlerHUD)}.tscn");
+
+    private AnimationPlayer animationPlayer;
+
+    public override void _Ready()
+    {
+      base._Ready();
+
+      animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+    }
+
+    public void Setup(IEnumerable<Battler> battlers)
+    {
+      foreach (var battler in battlers)
+      {
+        var battlerHud = uiBattlerHud.Instance<UIBattlerHUD>();
+        AddChild(battlerHud);
+        battlerHud.Setup(battler);
+      }
+    }
+
+    public void FadeIn()
+    {
+      animationPlayer.Play("fade_in");
+    }
+    
+    public void FadeOut()
+    {
+      animationPlayer.Play("fade_out");
+    }
+  }
+}
