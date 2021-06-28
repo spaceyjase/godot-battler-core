cc7634c6cdb178ab09d20caa8ba37ae65e05ebef
Jason <429978+spaceyjase@users.noreply.github.com>
2021-06-28 10:25:19 +0100

    Combat results WIP.

---
+++ Scripts/UI/UICombatResultPanel.cs
@@ -0,0 +1,45 @@
+using System.Security.Cryptography.X509Certificates;
+using Godot;
+
+namespace battler.Scripts.UI
+{
+  [Tool]
+  public class UICombatResultPanel : Panel
+  {
+    [Export] private string text;
+
+    private string Text
+    {
+      get => text;
+      set
+      {
+        text = value;
+        if (label != null)
+        {
+          label.Text = text;
+        }
+      }
+    }
+    
+    private Label label;
+    private AnimationPlayer animationPlayer;
+
+    public override void _Ready()
+    {
+      base._Ready();
+
+      label = GetNode<Label>("Label");
+      animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+    }
+
+    public void FadeIn()
+    {
+      animationPlayer.Play("fade_in");
+    }
+    
+    public void FadeOut()
+    {
+      animationPlayer.Play("fade_out");
+    }
+  }
+}
